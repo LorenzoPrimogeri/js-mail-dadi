118ea97986b793b497a75d057eb7343e450def25
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-03-09 17:47:40 +0100

    complete with style

--- main.js
+++ main.js
@@ -4,32 +4,33 @@ const genera = document.getElementById("Genera");
 let controllo = false;
 let verifica = document.getElementById("Verifica");
 botton.addEventListener('click', function () {
-
     const email = document.getElementById('userEmail').value;
     for (let i = 0; i < emailsarray.length && controllo == false; i++) {
         if (email == emailsarray[i]) {
-            verifica.innerHTML += `<span class="c-green">Verificato<\span>`
+            verifica.innerHTML = `<span class="c-green">Verificato<\span>`
             controllo = true;
-            email.value = " ";
         }
     }
     if (controllo == false) {
-        verifica.innerHTML += `<span class="c-red">Non Verificato<\span>`
-        email.value = " ";
+        verifica.innerHTML = `<span class="c-red">Non Verificato<\span>`
     }
 })
 genera.addEventListener('click', function () {
     if (controllo == true) {
         const randomPlayer = (Math.random() * 6).toFixed(1);
         const randomComputer = (Math.random() * 6).toFixed(1);
-        console.log(randomPlayer + ' ' + randomComputer);
+        const punteggioPlayer = document.getElementById("PunteggioP");
+        const punteggioComputer = document.getElementById("PunteggioC");
+        const risultato = document.getElementById("Risultato");
+        punteggioPlayer.innerHTML = randomPlayer;
+        punteggioComputer.innerHTML = randomComputer;
+
         if (randomPlayer > randomComputer) {
-            console.log("hai vinto");
+            risultato.innerHTML = "Hai vinto";
+            risultato.classList.add("c-green");
         } else {
-            console.log("hai perso");
+            risultato.innerHTML = "Hai perso";
+            risultato.classList.add("c-red");
         }
     }
 })
-
-
-//lprimogeri@gmail.com
