5e357de6022253c6688f06dd498b4610a8cb3c45
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-03-10 11:15:18 +0100

    fixing clear after verify, adding "pareggio"

--- main.js
+++ main.js
@@ -1,35 +1,45 @@
 const emailsarray = ["ciao", "ciao2", "ciao4", "ciao3", "ciao5", "l2primogeri@gmail.com", "l3primogeri@gmail.com"];
-const botton = document.getElementById("Btn");
+const button = document.getElementById("Btn");
 const genera = document.getElementById("Genera");
 let controllo = false;
 let verifica = document.getElementById("Verifica");
-botton.addEventListener('click', function () {
+button.addEventListener('click', function () {
+    controllo = false;
     const email = document.getElementById('userEmail').value;
     for (let i = 0; i < emailsarray.length && controllo == false; i++) {
         if (email == emailsarray[i]) {
-            verifica.innerHTML = `<span class="c-green">Verificato<\span>`
+            verifica.innerText = `Verificato`;
+            verifica.classList.add("c-green");
             controllo = true;
+            document.getElementById("userEmail").value = " ";
         }
     }
     if (controllo == false) {
-        verifica.innerHTML = `<span class="c-red">Non Verificato<\span>`
+        verifica.innerText = `Non Verificato`;
+        verifica.classList.add("c-red");
+        document.getElementById("userEmail").value = " ";
     }
 })
 genera.addEventListener('click', function () {
     if (controllo == true) {
-        const randomPlayer = (Math.random() * 6).toFixed(1);
-        const randomComputer = (Math.random() * 6).toFixed(1);
+        const randomPlayer = Math.floor(Math.random() * 6 + 1);
+        const randomComputer = Math.floor(Math.random() * 6 + 1);
         const punteggioPlayer = document.getElementById("PunteggioP");
         const punteggioComputer = document.getElementById("PunteggioC");
         const risultato = document.getElementById("Risultato");
         punteggioPlayer.innerHTML = randomPlayer;
         punteggioComputer.innerHTML = randomComputer;
-
         if (randomPlayer > randomComputer) {
             risultato.innerHTML = "Hai vinto";
+            risultato.classList.remove("c-red");
             risultato.classList.add("c-green");
+        } else if (randomComputer == randomPlayer) {
+            risultato.innerHTML = "pareggio";
+            risultato.classList.remove("c-green","c-red");
+            risultato.classList.add("c-grey");
         } else {
             risultato.innerHTML = "Hai perso";
+            risultato.classList.remove("c-green");
             risultato.classList.add("c-red");
         }
     }
